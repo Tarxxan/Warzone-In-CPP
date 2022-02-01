7abdf06cf8efc62c6739a007261cc61ec8e592da
Deokyeong Kim <hsad61@gmail.com>
2022-02-01 01:52:10 +0000

    Map.cpp edited online with Bitbucket

--- Map.cpp
+++ Map.cpp
@@ -9,76 +9,79 @@ using namespace std;
 //-----------------------------Territory Class--------------------------------------------------------
 //----------------------------------------------------------------------------------------------------
 
-    Territory::Territory()
-    {
-        //do we need to define default constructor?
-    }
+Territory::Territory()
+{
+    //do we need to define default constructor?
+}
 
-    Territory::Territory(int territoryId, string territoryName, string continentName, vector<Territory*> adjacentTerritories)
-    {
-        this->territoryId = territoryId;
-        this->territoryName = territoryName;
-        this->continentName = continentName;
-        this->adjacentTerritories = adjacentTerritories;
-    }
+Territory::Territory(int territoryId, string territoryName, string continentName, vector<Territory*> adjacentTerritories)
+{
+    this->territoryId = territoryId;
+    this->territoryName = territoryName;
+    this->continentName = continentName;
+    this->adjacentTerritories = adjacentTerritories;
+}
 
-    Territory::Territory(const Territory& territory)
-    {
-        this->territoryId = territory.territoryId;
-        this->territoryName = territory.territoryName;
-        this->continentName = territory.continentName;
-        this->numberOfArmies = territory.numberOfArmies;
-        this->adjacentTerritories = territory.adjacentTerritories;
-    }
+Territory::Territory(const Territory& territory)
+{
+    this->territoryId = territory.territoryId;
+    this->territoryName = territory.territoryName;
+    this->continentName = territory.continentName;
+    this->numberOfArmies = territory.numberOfArmies;
+    this->adjacentTerritories = territory.adjacentTerritories;
+}
 
-    Territory& Territory::operator=(const Territory& territory)
-    {
-        this->territoryId = territory.territoryId;
-        this->territoryName = territory.territoryName;
-        this->continentName = territory.continentName;
-        this->numberOfArmies = territory.numberOfArmies;
-        this->adjacentTerritories = territory.adjacentTerritories;
-    }
+Territory& Territory::operator=(const Territory& territory)
+{
+    this->territoryId = territory.territoryId;
+    this->territoryName = territory.territoryName;
+    this->continentName = territory.continentName;
+    this->numberOfArmies = territory.numberOfArmies;
+    this->adjacentTerritories = territory.adjacentTerritories;
+}
 
-    ostream& operator<<(ostream& output, const Territory& territory)
-    {
-        output << "--Territory ID: " << territory.territoryId << endl;
-        output << "--Territory Name: " << territory.territoryName << endl;
-        output << "--Continent Name: " << territory.continentName << endl;
-        output << "--Number of armies: " << territory.numberOfArmies << endl;
-        output << "--adjacent territories: " << endls;
-        //I want to iterate through the vector adjacentTerritories and print out naems. //----------------------------TODO
+ostream& operator<<(ostream& output, const Territory& territory)
+{
+    output << "--Territory ID: " << territory.territoryId << endl;
+    output << "--Territory Name: " << territory.territoryName << endl;
+    output << "--Continent Name: " << territory.continentName << endl;
+    output << "--Number of armies: " << territory.numberOfArmies << endl;
+    output << "--adjacent territories: " << endl;
+    //I want to iterate through the vector adjacentTerritories and print out naems. //----------------------------TODO
 
-    }
+}
 
 
-    int Territory::getTerritoryId()
-    {
-        return territoryId;
-    }
+int Territory::getTerritoryId()
+{
+    return territoryId;
+}
 
-    string Territory::getTerritoryName()
-    {
-        return territoryName;
-    }
+string Territory::getTerritoryName()
+{
+    return territoryName;
+}
 
-    string Territory::getContientName()
-    {
-        return continentName;
-    }
+string Territory::getContientName()
+{
+    return continentName;
+}
 
-    vector<Territory*> Territory::getAdjacentTerritories()
-    {
-        return adjacentTerritories; //this returns the vector
-    }
-    void Territory::setNumberOfArmies(int num)
-    {
-        numberOfArmies = num;
-    }
-    Player* Territory::getOwnerOfTerritory()
-    {
-        return ownerOfTerritory; // return the pointer to player object
-    }
+vector<Territory*> Territory::getAdjacentTerritories()
+{
+    return adjacentTerritories; //this returns the vector
+}
+void Territory::setNumberOfArmies(int num)
+{
+    numberOfArmies = num;
+}
+
+
+//-------------------------------------------------------------------NOTE: no idea why this doesnt work?
+//Player* Territory::getOwnerOfTerritory()
+//{
+//    return ownerOfTerritory; // return the pointer to player object
+//}
 
 
 
@@ -86,7 +89,6 @@ using namespace std;
 //----------------------------------------------------------------------------------------------------
 //-----------------------------Continent Class--------------------------------------------------------
 //----------------------------------------------------------------------------------------------------
-
 
 Continent::Continent()
 {
@@ -103,11 +105,11 @@ Continent::Continent(int continentId, string continentName, int continentArmyVal
 }
 
 //copy constructor
-Continent::Continent(Const Continent& continent)
+Continent::Continent(const Continent& continent)
 {
-    this->continentId = Continent.continentId;
-    this->continentName = Continent.continentName;
-    this->continentControlValue = continent.continentArmyValue;
+    this->continentId = continent.continentId;
+    this->continentName = continent.continentName;
+    this->continentControlValue = continent.continentControlValue;
     this->territories = continent.territories;
 }
 
@@ -121,14 +123,14 @@ Continent::~Continent()
 //assignment operator
 Continent& Continent::operator=(const Continent& continent)
 {
-    this->continentId = Continent.continentId;
-    this->continentName = Continent.continentName;
-    this->continentControlValue = continent.continentArmyValue;
+    this->continentId = continent.continentId;
+    this->continentName = continent.continentName;
+    this->continentControlValue = continent.continentControlValue;
     this->territories = continent.territories;
 }
 
 
- //stream insertion operator 
+//stream insertion operator 
 ostream& operator<<(ostream& output, const Continent& continent)
 {
     output << "--Continet ID: " << continent.continentId << endl;
@@ -137,6 +139,19 @@ ostream& operator<<(ostream& output, const Continent& continent)
     output << "--Territories: " << endl;
     // -------------------------------------------NOTE : want to print out all the territories belong to this continent
 }
+
+
+
+
+
+
+//----------------------------------------------------------------------------------------------------
+//-----------------------------Map Class--------------------------------------------------------------
+//----------------------------------------------------------------------------------------------------
+
+
+
+
 
 
 
@@ -149,68 +164,63 @@ ostream& operator<<(ostream& output, const Continent& continent)
 // Map loader creates a map object as a graph data structure.
 // Map loader should be able to read any text file (even invalid ones)
 // Give values to an object that is uninitialized
-MapLoader::MapLoader(){};
+MapLoader::MapLoader() {};
 
- MapLoader::MapLoader(const MapLoader &copyML){
-    FileContents=copyML.FileContents;
-    FileName=copyML.FileName;
+MapLoader::MapLoader(const MapLoader& copyML) {
+    FileContents = copyML.FileContents;
+    FileName = copyML.FileName;
     // not sure if this should be different because its a pointer but ill look into it
     map = copyML.map;
- }
+}
 
 //Used to change values of an already initialized object
-MapLoader& MapLoader::operator=(const MapLoader &AssignML){
-    FileContents=AssignML.FileContents;
-    FileName=AssignML.FileName;
+MapLoader& MapLoader::operator=(const MapLoader& AssignML) {
+    FileContents = AssignML.FileContents;
+    FileName = AssignML.FileName;
     map = AssignML.map;
     return *this;
 
-    }
+}
 
 //  MapLoader will actually be reading the file
 
-MapLoader::MapLoader(string FileName){
+MapLoader::MapLoader(string FileName) {
 
-ifstream in(FileName); 
-string TempText="";
+    ifstream in(FileName);
+    string TempText = "";
 
-if(in.fail())
-{
-   cout <<" An Error has occured when reading from the file. Unexpected value or file error." << endl;
-   // delete pointer to map maybe inside of here 
+    if (in.fail())
+    {
+        cout << " An Error has occured when reading from the file. Unexpected value or file error." << endl;
+        // delete pointer to map maybe inside of here 
 
-return;
-}  
+        return;
+    }
 
-while(getline(in,TempText)){
+    while (getline(in, TempText)) {
 
-FileContents+=TempText +"\n";
+        FileContents += TempText + "\n";
 
-}
+    }
 
-in.close();
-// Testing that we can display the file contents
-this->FileContents=FileContents;
+    in.close();
+    // Testing that we can display the file contents
+    this->FileContents = FileContents;
 }
 
 // IO Stream Operators for MapLoader
 //Figured out what they do just unsure what we will put in them.
 
-istream &operator >> (istream &in,  MapLoader &ML)
+istream& operator >> (istream& in, MapLoader& ML)
 {
     cout << "Enter Map File";
     in >> ML.FileName;
     return in;
-    }
+}
 
-ostream &operator << (ostream &out, const MapLoader &ML)
+ostream& operator << (ostream& out, const MapLoader& ML)
 {
     out << ML.FileContents << endl;
     return out;
 }
-
-
-//----------------------------------------------------------------------------------------------------
-//-----------------------------Map Class--------------------------------------------------------------
-//----------------------------------------------------------------------------------------------------
 
