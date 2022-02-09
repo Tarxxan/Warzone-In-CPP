1de7c77ee6afb02d3f208276b1f24d09fe648c95
Christian Henneveld <christian.henneveld@continent8.com>
2022-02-09 16:25:12 -0500

    Minor Changes in Map files. Deleted MapLoader class and header. Added more maps to test. Seems to be working how we want it to. Adjusted MapDriver to something more usable so only minor additions will be needed to finsih it

--- MapDriver.cpp
+++ MapDriver.cpp
@@ -4,52 +4,67 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+int main()
+{
 
-
-    
-    //MapLoader ml("bigeurope.map");
+    // MapLoader ml("bigeurope.map");
 
     // cout <<*ml.map->getAllContinents().at(1)->getTerritories().at(1);
-    //cout<< ml.map->oneContinent();
-    //cout<< ml.map->connectedSubgraphs();
+    // cout<< ml.map->oneContinent();
+    // cout<< ml.map->connectedSubgraphs();
     // ml.map->connectedGraph();
-    //ml.map->validate();
-
-
-
+    // ml.map->validate();
 
     //-----------------------------------added--------------------------------------------------------------
+    bool playAgain = true;
+    while (playAgain)
+    {
+        cout << "Type the Name of a map. We will attempt to validate it." << endl;
+        string s = {};
+        cin >> s;
+
+        MapLoader ml2(s);
+        ml2.map->validate();
+
+        cout << "\nWould you like to check another Map (Y/N)" << endl;
+        string retry = {};
+        cin >> retry;
+        if (retry.compare("N") == 0)
+        {
+            playAgain = false;
+        }
+    }
+
+    cout << "The following operations are done on the Big Europe Map(valid)" << endl;
+
     MapLoader ml("bigeurope.map");
+    // testing territory insertion operator.
+    cout << endl
 
-    //testing territory insertion operator. 
-    cout << endl<< "[Testing Territory insertion operator]" << endl;
-    vector<Territory*> myTerrs = (ml.map->getAllContinents().front())->getTerritories();
+         << "\n\n[Testing Territory insertion operator]" << endl;
+    vector<Territory *> myTerrs = (ml.map->getAllContinents().front())->getTerritories();
 
-    for (Territory* t : myTerrs) 
-    { cout << *t; }
+    for (Territory *t : myTerrs)
+    {
+        cout << *t;
+    }
 
+    // testing continent insertion operator.
+    cout << endl
+         << "\n\n[Testing continent insertion operator]" << endl;
+    vector<Continent *> myConts = ml.map->getAllContinents();
+    for (Continent *c : myConts)
+    {
+        cout << *c;
+    }
 
-    //testing continent insertion operator.
-    cout <<endl<< "[Testing continent insertion operator]" << endl;
-    vector<Continent*> myConts = ml.map->getAllContinents();
-    for (Continent* c : myConts)
-    { cout << *c; }
-     
-
-    //testing Map insertion operator
-    cout << endl << "[Testing Map insertion operator]" << endl;
-    cout << *ml.map;    
+    // testing Map insertion operator
+    cout << endl
+         << "\n\n[Testing Map insertion operator]" << endl;
+    cout << *ml.map;
     //-------------------------------------------------------------------------------------------------
 
+    system("pause");
 
-    //string CurrentMap="";
-    //cout << "Which Map would you like to play"<<endl;
-    //cin >> CurrentMap;
-
-    //MapLoader ML(CurrentMap);
-    //cout << ML;
-    //system("pause");
-    
     return 0;
 }
