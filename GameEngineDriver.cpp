8ba34f4caa2df7091a391932615c9d49ba57e0a6
Deokyeong Kim <hsad61@gmail.com>
2022-02-13 15:39:14 +0000

    GameEngineDriver.cpp commented, done.

--- GameEngineDriver.cpp
+++ GameEngineDriver.cpp
@@ -14,17 +14,24 @@ using namespace std;
 int main()
 {
     GameEngine GM;
-
-    GM.start();
     string command;
+
+
+    //start of game
+    GM.start();
+    
+
+
+    cout << "\n>>Welcome to Warzone!";
     while (true)
     {
+        //load map
         while (GM.getTrans().compare("startup") == 0)
         {
 
             string mapName;
 
-            cout << "Welcome to Warzone! What what you like to do.( Ex. loadmap )" << endl;
+            cout << "\n>>What what you like to do.( Ex. loadmap )" << endl;
             cin >> command;
 
             if (command.compare("loadmap") == 0)
@@ -32,36 +39,37 @@ int main()
                 GM.setState("maploaded");
             }
             else
-                cout << "You must start by loading the map" << endl;
+                cout << "\n>>You must start by loading the map" << endl;
 
+            // Ask user map file name
             while (GM.getState().compare("maploaded") == 0)
             {
-                cout << " Whats the name of the map you want to load? :(Ex. bigeurope.map)" << endl;
+                cout << "\n>>Whats the name of the map you want to load? :(Ex. bigeurope.map)" << endl;
                 cin >> mapName;
-                cout << "You have selected " << mapName << " would you like to validate the map or load another one? : (Ex. loadmap, validatemap )" << endl;
+                cout << "\n>>You have selected " << mapName << " would you like to validate the map or load another one? : (Ex. loadmap, validatemap )" << endl;
                 cin >> command;
                 if (command.compare("validatemap") == 0)
                 {
                     GM.validatemap();
                     // Call explicitly to not forget to change state in real implementation
                     GM.setState("mapvalidated");
+                    cout << "\n>>Validated map... " << endl;
                 }
                 else
-                    cout << "Returning to load another map" << endl;
+                    cout << "\n>>Returning to load another map..." << endl;
             }
-            if(GM.getState().compare("mapvalidated")==0){
-                cout << "Validated map... " << endl;
-            }
-            
 
+
+
+            // Ask user number of players
             while (GM.getState().compare("mapvalidated") == 0)
             {
                 int playersPlaying;
-                cout << "How many players will be playing Warzone? :" << endl;
+                cout << ">>How many players will be playing Warzone? :" << endl;
                 cin >> playersPlaying;
                 if (playersPlaying > 1)
                 {
-                    cout << "You entered " << playersPlaying << " if this is correct, type \"addplayer\". If not type anything." << endl;
+                    cout << "\n>>You entered " << playersPlaying << " if this is correct, type \"addplayer\". If not type anything." << endl;
                     cin >> command;
                     if (command.compare("addplayer") == 0)
                     {
@@ -72,14 +80,17 @@ int main()
                     }
                 }
                 else
-                    cout << "You must have atleast two players to play" << endl;
+                    cout << "\n>>You must have atleast two players to play" << endl;
             }
 
+            // Adding players 
             while (GM.getState().compare("players added") == 0)
             {
-                cout << "Are you sure " << GM.getPlayers() << " is the correct amount of players?" << endl;
-                cout << "Type \"assigncountries\" to proceed or \"addplayer\" to change the players." << endl;
+                cout << "\n>>Are you sure " << GM.getPlayers() << " is the correct amount of players?" << endl;
+                cout << ">>Type \"assigncountries\" to proceed or \"addplayer\" to change the players." << endl;
                 cin >> command;
+
+                // Assign reinforcement
                 if (command.compare("assigncountries") == 0)
                 {
                     GM.assignCountries();
@@ -87,10 +98,10 @@ int main()
                     GM.setTrans("play");
                 }
                 else if (command.compare("addplayer") == 0)
-                {
+                { // Reset player number 
 
                     int playersPlaying;
-                    cout << "How many players will be playing Warzone? :" << endl;
+                    cout << "\n>>How many players will be playing Warzone? :" << endl;
                     cin >> playersPlaying;
                     if (playersPlaying > 1)
                     {
@@ -98,22 +109,23 @@ int main()
                         GM.setPlayers(playersPlaying);
                     }
                     else
-                        cout << "You must have atleast two players to play" << endl;
+                        cout << "\n>>You must have atleast two players to play" << endl;
                 }
                 else
-                    cout << "Invalid input. Asking for player count again" << endl;
+                    cout << "\n>>Invalid input. Asking for player count again" << endl;
             }
         }
-         if(GM.getState().compare("play")==0){
-                cout << "Validated map... " << endl;
-            }
-        
+   
+
+
+        // In Play transition 
         while (GM.getTrans().compare("play") == 0)
         {   
 
+            // start of Play phase
             while (GM.getState().compare("assign reinforcement") == 0)
             {
-                cout << "Type \"issueorder\" to start playing" << endl;
+                cout << "\n>>Type \"issueorder\" to start playing" << endl;
                 cin >> command;
 
                 if (command.compare("issueorder") == 0)
@@ -121,40 +133,50 @@ int main()
                     GM.setState("issue orders");
                 }
                 else
-                    cout << "Invalid input. lets try this again." << endl;
+                    cout << "\n>>Invalid input. lets try this again." << endl;
             }
 
+
+            // Issue order
             while (GM.getState().compare("issue orders") == 0)
             {
-                cout << "Each player will now get a turn to issue orders" << endl;
+                cout << "\n>>Each player will now get a turn to issue orders" << endl;
                 for (int i = 0; i < GM.getPlayers(); i++)
                 {
                     GM.issueOrders();
+
+                    cout << ">>Player " << (i+1) << " is issuing orders..." << endl;
+                   
+
                 }
-                cout << "Type \"endissueorders\" to proceed or \"issueorder\" to restart" << endl;
+                cout << "\n>>Type \"endissueorders\" to proceed or \"issueorder\" to restart" << endl;
                 cin >> command;
                 if (command.compare("endissueorders") == 0)
                 {
                     GM.setState("execute orders");
                 }
                 else
-                    cout << "Going back to re-issue orders." << endl;
+                    cout << "\n>>Going back to re-issue orders..." << endl;
             }
 
+            // execute orders
             while (GM.getState().compare("execute orders") == 0)
             {
-                cout << "Each player will now get a turn to execute orders" << endl;
+                cout << "\n>>Each player will now get a turn to execute orders" << endl;
                 for (int i = 0; i < GM.getPlayers(); i++)
                 {
                     GM.executeOrders();
+                    cout << ">>Executing orders..." << endl;
                 }
-                cout << "Type \"endexecorders\" to proceed \"win\" to check win or \"execorder\" to restart" << endl;
+                cout << "\n>>Type \"endexecorders\" to proceed \"win\" to check win or \"execorder\" to restart" << endl;
                 cin >> command;
                 if (command.compare("endexecorders") == 0)
                 {
-                    GM.setState("assign reinforcement");
+                    GM.setState("issue orders");
                 }
 
+
+                // win state 
                 else if (command.compare("win")== 0)
                 {
 
@@ -162,31 +184,37 @@ int main()
                     {
                         GM.setState("win");
                     }
-                    else
-                        cout << "You didnt win yet. Keep playing" << endl;
-                        GM.setPlayers(1);
+                    else {
+                        cout << "\n>>You didnt win yet. Keep playing" << endl;
+                        // reduce player number by 1 when 'win' state is checked
+                        GM.setPlayers(GM.getPlayers()-1);
+                    }
+                        
                 }
                 else
-                    cout << "Going back to re-execute orders." << endl;
+                    cout << "\n>>Going back to re-execute orders." << endl;
             }
 
+            // win message 
             while (GM.getState().compare("win") == 0)
-            {   cout << "CONGRATS" << endl;
-                cout << " Would you like to play again?Type \"play\" for yes and \"end\" to exit" << endl;
+            {   cout << "\n>>CONGRATS" << endl;
+                cout << ">>Would you like to play again? Type \"play\" for yes and \"end\" to exit" << endl;
                 cin >> command;
                 if (command.compare("play") == 0)
                 {
+                    // going back to start of game
                     GM.setTrans("startup");
                     GM.setState("start");
                 }
                 else if(command.compare("end") == 0)
                 {
-                    cout << "Thank you for playing warzone." << endl;
+                    // end game
+                    cout << ">>Thank you for playing warzone." << endl;
                     system("pause");
                     return 0;
                 }
                 else 
-                    cout <<"Invalid response... JUST CHOOSE ONE"<< endl;
+                    cout <<"\n>>Invalid response... JUST CHOOSE ONE"<< endl;
             }
         }
     
