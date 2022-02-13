6dd2c912535aafeb7c29f5e47d22f69e7c8ed6e7
Mark O'Shaughnessy <mark@pop-os.localdomain>
2022-02-13 12:34:42 -0500

    testing for the cards driver functions

--- Cards.cpp
+++ Cards.cpp
@@ -1,82 +1,91 @@
 #include "Cards.h"
 
-
-
-
-
+//Dummy Order class
 
 Order::Order(){};
-Order::Order(string type){
+Order::Order(string type)
+{
     this->name = type;
     this->description = type + " Order";
 }
-Order::Order(const Order& order){
+Order::Order(const Order &order)
+{
     this->name = order.name;
     this->description = order.description;
     this->effect = order.effect;
-    // this->player = order.player;
+    this->player = order.player;
 }
-Order::~Order(){}
-Order& Order::operator=(const Order& o){
+Order::~Order() {}
+Order &Order::operator=(const Order &o)
+{
     this->name = o.name;
     this->description = o.description;
     this->effect = o.effect;
-    // this->player = o.player;
+    this->player = o.player;
     return *this;
 }
 //Assignment operator // It can be used to create an object just like the copy constructor
-string Order::getName(){
+string Order::getName()
+{
     return this->name;
 }
-string Order::getDescription(){
+string Order::getDescription()
+{
     return this->description;
 }
-string Order::getEffect(){
+string Order::getEffect()
+{
     return this->effect;
 }
-// Player* Order::getPlayer(){
-//     return this->player;
-// }
-bool Order::validate(){return true;}
-bool Order::execute(){return true;}
-std::ostream& operator<<(ostream& strm,Order& o){
+Player *Order::getPlayer()
+{
+    return this->player;
+}
+bool Order::validate() { return true; }
+bool Order::execute() { return true; }
+std::ostream &operator<<(ostream &strm, Order &o)
+{
     return strm << "Order: " << o.getDescription() << o.getEffect();
 }
-
-
-
-
-
 
 // DUMMY ORDER LIST
 
 OrderList::OrderList(){};
 OrderList::~OrderList(){};
-OrderList& OrderList::operator=(const OrderList& oList){
+OrderList &OrderList::operator=(const OrderList &oList)
+{
     this->orderList = oList.orderList;
     return *this;
 }
-vector <Order*> OrderList::getOrders(){
+vector<Order *> OrderList::getOrders()
+{
     return this->orderList;
 }
-OrderList::OrderList(const OrderList &copyOL){
+OrderList::OrderList(const OrderList &copyOL)
+{
     this->orderList = copyOL.orderList;
 }
-std::ostream& operator << (std::ostream& strm,const OrderList& orderList) {
-    for(Order* order : orderList.orderList){
+std::ostream &operator<<(std::ostream &strm, const OrderList &orderList)
+{
+    for (Order *order : orderList.orderList)
+    {
         cout << *order << endl;
     }
-  
+
     return strm << "";
 }
-void OrderList::push(Order* order){
+void OrderList::push(Order *order)
+{
     orderList.push_back(order);
 }
-int OrderList::remove(Order *actual){
+int OrderList::remove(Order *actual)
+{
     int index = 0;
-    for(Order* order : this->orderList){
-        if(order == actual){
-            auto posIt = this->orderList.begin()+index;
+    for (Order *order : this->orderList)
+    {
+        if (order == actual)
+        {
+            auto posIt = this->orderList.begin() + index;
             this->orderList.erase(posIt); // will delete the thing we are looking for
             return index;
         }
@@ -85,129 +94,162 @@ int OrderList::remove(Order *actual){
     return -1;
 }
 // So if True it will move up if false it will move down
-bool OrderList::move(Order *order, bool moveUp){
+bool OrderList::move(Order *order, bool moveUp)
+{
     int index = remove(order);
-    if (index == -1){
-        cout << "Could not find the element in the list" <<endl;
+    if (index == -1)
+    {
+        cout << "Could not find the element in the list" << endl;
         return false;
     }
     // If we try to move the first up
-    if (index == 0 && (moveUp || this->orderList.size() == 0)){
+    if (index == 0 && (moveUp || this->orderList.size() == 0))
+    {
         this->orderList.insert(this->orderList.begin(), order);
         return true;
     }
     //if we try to move the last one down
-    if (index == this->orderList.size() && !moveUp){
+    if (index == this->orderList.size() && !moveUp)
+    {
         push(order);
         return true;
     }
-    if (moveUp){
-        this->orderList.insert(this->orderList.begin()+index-1, order);
+    if (moveUp)
+    {
+        this->orderList.insert(this->orderList.begin() + index - 1, order);
     }
-    else{
-        this->orderList.insert(this->orderList.begin()+index+1, order);
+    else
+    {
+        this->orderList.insert(this->orderList.begin() + index + 1, order);
     }
     return true;
 }
 
 // This is a more use friendly methods for move which will call move
-bool OrderList::moveUp(Order *order){
+bool OrderList::moveUp(Order *order)
+{
     return move(order, true);
 }
-bool OrderList::moveDown(Order *order){
+bool OrderList::moveDown(Order *order)
+{
     return move(order, false);
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 ////////////////////////////////////////// CARD //////////////////////////////////////////////////////
-Card::Card(string type){
+
+Card::Card(string type) //parameter constructor
+{
     this->type = type;
 }
-Card::~Card(){};
-Card::Card(const Card& card){
+
+Card::~Card() //deconstructor
+{
+    cout << "Card Deconstructor Called\n";
+};
+Card::Card(const Card &card) //copy constructor
+{
     this->type = card.type;
-    // this->player = card.player;
+    this->player = card.player;
 }
-Card& Card::operator=(const Card& card){
-    // this->player = card.player;
+Card &Card::operator=(const Card &card) //assignment operator
+{
+    this->player = card.player;
     this->type = card.type;
     return *this;
 }
-void Card::setPlayer(Player* player){
+void Card::setPlayer(Player *player) //assign player to Card
+{
     this->player = player;
 }
-void Card::play(Deck* deck){
+void Card::play(Deck *deck) //play the current card by creating a new order, adding the order to the players orderlist.
+                            // remove the current card from the players hand and add it back to the deck;
+{
+
     this->player->hand->remove(this);
     cout << this->type << " Card played!\n";
-    Order* newOrder = new Order(this->type);
+    Order *newOrder = new Order(this->type);
     this->player->orders->push(newOrder);
     deck->push(this);
-
 }
-string Card::getType(){
+string Card::getType()
+{
     return this->type;
 }
-string Card::getPlayerName(){
-    return "";
-    // return this->player->getName(); // the get Nme doesnt exist yet
+string Card::getPlayerName() // get name of owner of this card
+{
+    return this->player->name;
 }
-std::ostream& operator<<(ostream& strm, Card& card){
-    return strm << "Card type: " << card.getType() << card.getPlayerName();
+std::ostream &operator<<(ostream &strm, Card &card) //print the card's type and the current owner;
+{
+    return strm << "Card type: " << card.getType() << "\tOwner: " << card.getPlayerName();
 }
-
 
 //////////////////////////////////// HAND ///////////////////////////////////////////////////////
-Hand::Hand(){};
-vector <Card*> Hand::getHand(){
-    return this->hand;
-}
-Hand::Hand(const Hand& handO){
+
+Hand::Hand(){}; //empty constructor
+
+Hand::Hand(const Hand &handO) //copy constructor
+{
     this->hand = handO.hand;
 }
-void Hand::push(Card* card){
+Hand::~Hand() //deconstructor
+{
+    if (this->hand.size() != 0)
+    {
+        for (int i = 0; i < hand.size(); i++)
+        {
+            delete hand.at(i);
+        }
+    }
+
+    hand.clear();
+    cout << "Hand Deconstructor Called";
+}
+void Hand::push(Card *card) //add card to current hand
+{
     this->hand.push_back(card);
 };
-void Hand::remove(Card* actual){
+void Hand::remove(Card *actual) //remove card from current hand
+{
     int index = 0;
-    for(Card* card : this->hand){
-        if(card == actual){
-            auto posIt = this->hand.begin()+index;
+    for (Card *card : this->hand)
+    {
+        if (card == actual)
+        {
+            auto posIt = this->hand.begin() + index;
             this->hand.erase(posIt); // will delete the thing we are looking for
             return;
         }
-        index ++;
-    }
-}
-void Hand::fill(Deck* deck){
-    for(int i = 0;i<5;i++){
-        this->hand.push_back(deck->dummyDraw());
+        index++;
     }
 }
 
+vector<Card *> Hand::getHand()
+{ //return the vector of cards representing the hand
+    return this->hand;
+}
 
+void Hand::chooseCard(string orderType, Deck *deck) //select card and play it by inputing the type
+{
+    for (Card *card : hand)
+    {
+        if (card->getType() == orderType)
+        {
+            card->play(deck);
+            return;
+        }
+    }
+    cout << "ORDER TYPE NOT FOUND\n";
+}
 
-std::ostream& operator << (ostream& strm,const Hand& hO){
-    for(Card* card : hO.hand){
+std::ostream &operator<<(ostream &strm, const Hand &hO) // print out each card in the hand
+{
+    for (Card *card : hO.hand)
+    {
         cout << *card << endl;
     }
     return strm << "";
 }
-
-
 
 // Dummy Player class
 Player::Player()
@@ -225,112 +267,113 @@ void Player::setPlayerName(string s)
 {
     this->name = s;
 }
-void Player::setHand(Hand* newHand){
+void Player::setHand(Hand *newHand)
+{
     this->hand = newHand;
 }
-Player::~Player(){}
+Player::~Player()
+{
+    delete this->hand;
+    cout << "Player Deconstructor Called\n";
+}
 
 ostream &operator<<(ostream &output, const Player &p)
 {
     output << "--Player Name: " << p.name << endl;
     return output;
 }
-void Player::addCard(Card* card) {
+void Player::addCard(Card *card)
+{
     this->hand->push(card);
 }
-void Player::fillHand(Deck* deck){
-    for(int i = 0;i<5;i++){
-        Card* temp = deck->dummyDraw();
-        temp->setPlayer(this);
-        this->hand->push(temp);
-    }
-}
-void Player::chooseCard(string orderType, Deck* deck){
-    for(Card* card : this->hand->getHand()){
-        if(card->getType() == orderType){
-            card->play(deck);
-            return;
-        }
-    }
-    cout << "ORDER TYPE NOT FOUND\n";
-}
-
 
 /////////////////////////////////////// DECK //////////////////////////////////////////////////////
-Deck::Deck(){};
-vector <Card*> Deck::getDeck(){
+Deck::Deck(){};                //empty constructor
+vector<Card *> Deck::getDeck() //return vector of cards representing the entire deck
+{
     return this->deck;
 }
 // Deck::Deck(const Deck& deckO){
 //     this->deck = deckO.getDeck();
 // }
-void Deck::initalizeDeck(){
-    for(int i = 0; i< 6;i++){
-        Card* tempCard = new Card("bomb");
-        this->deck.push_back(tempCard);
-    }
-    for(int i = 0; i< 6;i++){
-        Card* tempCard = new Card("reinforcement");
-        this->deck.push_back(tempCard);
-    }
-    for(int i = 0; i< 6;i++){
-        Card* tempCard = new Card("blockade");
-        this->deck.push_back(tempCard);
-    }
-    for(int i = 0; i< 6;i++){
-        Card* tempCard = new Card("blockade");
-        this->deck.push_back(tempCard);
-    }
-    for(int i = 0; i< 6;i++){
-        Card* tempCard = new Card("airlift");
-        this->deck.push_back(tempCard);
-    }
-    for(int i = 0; i< 6;i++){
-        Card* tempCard = new Card("diplomacy");
-        this->deck.push_back(tempCard);
-    }
 
+Deck::~Deck()
+{ //deconstructor
+    for (int i = 0; i < deck.size(); i++)
+    {
+        delete deck.at(i);
+    }
+    deck.clear();
+
+    cout << "Deck Deconstructor called\n";
 }
-void Deck::push(Card* card){
+void Deck::initalizeDeck() //populate the entire deck with 6 cards of each type;
+{
+    for (int i = 0; i < 6; i++)
+    {
+        Card *tempCard = new Card("bomb");
+        this->deck.push_back(tempCard);
+    }
+    for (int i = 0; i < 6; i++)
+    {
+        Card *tempCard = new Card("reinforcement");
+        this->deck.push_back(tempCard);
+    }
+    for (int i = 0; i < 6; i++)
+    {
+        Card *tempCard = new Card("blockade");
+        this->deck.push_back(tempCard);
+    }
+    for (int i = 0; i < 6; i++)
+    {
+        Card *tempCard = new Card("blockade");
+        this->deck.push_back(tempCard);
+    }
+    for (int i = 0; i < 6; i++)
+    {
+        Card *tempCard = new Card("airlift");
+        this->deck.push_back(tempCard);
+    }
+    for (int i = 0; i < 6; i++)
+    {
+        Card *tempCard = new Card("diplomacy");
+        this->deck.push_back(tempCard);
+    }
+}
+void Deck::push(Card *card) //add card to the deck
+{
     this->deck.push_back(card);
 }
-void Deck::draw(Player* player){
-    int index = rand() % this->deck.size();
-    auto posIt = this->deck.begin()+index;
-
-    Card* temp = this->deck.at(index);
-    this->deck.erase(posIt);
-    player->addCard(temp);
-    // gets a random card, rem  oves it from the list and assigns it to the players hand
-}
-Card* Deck::dummyDraw(){
+void Deck::draw(Player *player) //draw a random card from the deck and add it to the player's hand;
+{
     srand(time(NULL));
     int index = rand() % this->deck.size();
-    auto posIt = this->deck.begin()+index;
-    Card* temp = this->deck.at(index);
+    auto posIt = this->deck.begin() + index;
+    Card *temp = this->deck.at(index);
     this->deck.erase(posIt);
-    return temp;
+    temp->setPlayer(player);
+    player->addCard(temp);
 }
-void Deck::remove(Card* actual){
+
+void Deck::remove(Card *actual) //remove specific card from the deck
+{
     int index = 0;
-    for(Card* card : this->deck){
-        if(card == actual){
-            auto posIt = this->deck.begin()+index;
+    for (Card *card : this->deck)
+    {
+        if (card == actual)
+        {
+            auto posIt = this->deck.begin() + index;
             this->deck.erase(posIt); // will delete the thing we are looking for
             return;
         }
-        index ++;
+        index++;
     }
 }
-std::ostream& operator << (ostream& strm,const Deck& dO){
-    for(Card* card : dO.deck){
+std::ostream &operator<<(ostream &strm, const Deck &dO) // print out each card in the deck;
+{
+    for (Card *card : dO.deck)
+    {
         cout << *card << endl;
     }
     return strm << "";
 }
-
-
-
-
-
-
--- CardsDriver.cpp
+++ CardsDriver.cpp
@@ -2,46 +2,81 @@
 // filled by drawing cards from the deck, then calls the play method of all cards in the hand.
 #include <iostream>
 #include "Cards.h"
-int main() {
-    
-    Player* player1 = new Player("Mark");
-    Deck* deck = new Deck();
-    Hand* newHand = new Hand();
-    string cardInput;
+int main()
+{
+
+    Player *player1 = new Player("Mark");
+    Deck *deck = new Deck();
+    Hand *newHand = new Hand();
+
     deck->initalizeDeck();
     cout << "INITIAL DECK SIZE: " << deck->getDeck().size() << endl;
     player1->setHand(newHand);
-    player1->fillHand(deck);
-    cout << "Filling " << player1->name<< "'s Hand with Cards\nCARDS IN HAND: \n";
+    
+    cout << "DRAWING CARDS TO FILL HAND\n";
+    deck->draw(player1);
+    cout << "DREW 1 CARD\t HAND SIZE: " << player1->hand->getHand().size() << endl;
+    deck->draw(player1);
+    cout << "DREW 1 CARD\t HAND SIZE: " << player1->hand->getHand().size() << endl;
+    deck->draw(player1);
+    cout << "DREW 1 CARD\t HAND SIZE: " << player1->hand->getHand().size() << endl;
+    deck->draw(player1);
+    cout << "DREW 1 CARD\t HAND SIZE: " << player1->hand->getHand().size() << endl;
+    deck->draw(player1);
+    cout << "DREW 1 CARD\t HAND SIZE: " << player1->hand->getHand().size() << endl;
+    cout << "-------------------------------\n";
+    cout << "CARDS IN HAND: \n";
     cout << *player1->hand;
+    cout << "-------------------------------\n";
     cout << "~~~DECK SIZE~~~: " << deck->getDeck().size() << endl;
     cout << "~~~HAND SIZE~~~: " << player1->hand->getHand().size() << endl;
-    cout << "CHOOSE CARD: ";
-    cin >> cardInput;
-    player1->chooseCard(cardInput, deck);
-    cout << "~~~DECK SIZE~~~: " << deck->getDeck().size() << endl;
-    cout << "~~~HAND SIZE~~~: " << player1->hand->getHand().size() << endl;
-    cout << "CHOOSE CARD: ";
-    cin >> cardInput;
-    player1->chooseCard(cardInput, deck);
-    cout << "~~~DECK SIZE~~~: " << deck->getDeck().size() << endl;
-    cout << "~~~HAND SIZE~~~: " << player1->hand->getHand().size() << endl;
-    cout << "CHOOSE CARD: ";
-    cin >> cardInput;
-    player1->chooseCard(cardInput, deck);
-    cout << "~~~DECK SIZE~~~: " << deck->getDeck().size() << endl;
-    cout << "~~~HAND SIZE~~~: " << player1->hand->getHand().size() << endl;
-    cout << "CHOOSE CARD: ";
-    cin >> cardInput;
-    player1->chooseCard(cardInput, deck);
-    cout << "~~~DECK SIZE~~~: " << deck->getDeck().size() << endl;
-    cout << "~~~HAND SIZE~~~: " << player1->hand->getHand().size() << endl;
-    cout << "CHOOSE CARD: ";
-    cin >> cardInput;
-    player1->chooseCard(cardInput, deck);
-    cout << "~~~DECK SIZE~~~: " << deck->getDeck().size() << endl;
-    cout << "~~~HAND SIZE~~~: " << player1->hand->getHand().size() << endl;
-    cout << "------------------------\n";
-    cout << player1->name << "'s OrderList: \n" << *player1->orders;
+    cout << "-------------------------------\n";
 
+    Card *currentCard = player1->hand->getHand().at(0);
+    cout << "~~~PLAYING CARD IN HAND~~~\n";
+    currentCard->play(deck);
+    cout << "-------------------------------\n";
+    cout << "~~~DECK SIZE~~~: " << deck->getDeck().size() << endl;
+    cout << "~~~HAND SIZE~~~: " << player1->hand->getHand().size() << endl;
+    cout << "-------------------------------\n";
+
+    currentCard = player1->hand->getHand().at(0);
+    cout << "~~~PLAYING CARD IN HAND~~~\n";
+    currentCard->play(deck);
+    cout << "-------------------------------\n";
+    cout << "~~~DECK SIZE~~~: " << deck->getDeck().size() << endl;
+    cout << "~~~HAND SIZE~~~: " << player1->hand->getHand().size() << endl;
+    cout << "-------------------------------\n";
+
+    currentCard = player1->hand->getHand().at(0);
+    cout << "~~~PLAYING CARD IN HAND~~~\n";
+    currentCard->play(deck);
+    cout << "-------------------------------\n";
+    cout << "~~~DECK SIZE~~~: " << deck->getDeck().size() << endl;
+    cout << "~~~HAND SIZE~~~: " << player1->hand->getHand().size() << endl;
+    cout << "-------------------------------\n";
+
+    currentCard = player1->hand->getHand().at(0);
+    cout << "~~~PLAYING CARD IN HAND~~~\n";
+    currentCard->play(deck);
+    cout << "-------------------------------\n";
+    cout << "~~~DECK SIZE~~~: " << deck->getDeck().size() << endl;
+    cout << "~~~HAND SIZE~~~: " << player1->hand->getHand().size() << endl;
+    cout << "-------------------------------\n";
+
+    currentCard = player1->hand->getHand().at(0);
+    cout << "~~~PLAYING CARD IN HAND~~~\n";
+    currentCard->play(deck);
+    cout << "-------------------------------\n";
+
+    cout << "~~~DECK SIZE~~~: " << deck->getDeck().size() << endl;
+    cout << "~~~HAND SIZE~~~: " << player1->hand->getHand().size() << endl;
+    cout << "-------------------------------\n";
+    delete deck;
+    cout << "------------------------\n";
+    cout << "DISPLAYING PLAYER ORDERLIST\n";
+    cout << player1->name << "'s OrderList: \n"
+         << *player1->orders;
+    cout << "-------------------------------\n";
+    delete player1;
 }
